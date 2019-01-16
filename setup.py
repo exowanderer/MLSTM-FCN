4da2485bd4f05d3f81a0dfd0fbdda85a021c8a2f
Jonathan <jdfraine@gmail.com>
2019-01-16 14:38:18 +0200

    new name change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='mlstm_fcn',
+setup(name='mlstmfcn',
       version=0.1,
       # cmdclass=versioneer.get_cmdclass(),
       description='Multivariate LSTM Fully Convolutional Networks for Time Series Classification',
@@ -13,7 +13,7 @@ setup(name='mlstm_fcn',
       author="Somshubra Majumdar(titu1994), fazlekarim, "\
               "Jonathan Fraine (exowanderer)",
       packages=['mlstm_fcn'],
-      install_requires=['tensorflow-gpu>=1.4.0', 'keras>=2.1.2', 'scipy', 
+      install_requires=['tensorflow>=1.4.0', 'keras>=2.1.2', 'scipy', 
                         'numpy', 'numpy', 'pandas',
                         'scikit-learn>=0.18.2', 'h5py'],
       extras_require={'plots':  ["matplotlib"]}
