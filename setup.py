faa527b7a06f8c0b0f658fe597276f4958ac7839
Jonathan <jdfraine@gmail.com>
2019-01-16 14:30:15 +0200

    moved files around for package setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='mlstm_fcn',
+      version=0.1,
+      # cmdclass=versioneer.get_cmdclass(),
+      description='Multivariate LSTM Fully Convolutional Networks for Time Series Classification',
+      long_description=open('README.md').read(),
+      url='https://github.com/exowanderer/mlstm_fcn',
+      license='GPL3',
+      author="Somshubra Majumdar(titu1994), fazlekarim, "\
+              "Jonathan Fraine (exowanderer)",
+      packages=['mlstm_fcn'],
+      install_requires=['tensorflow-gpu>=1.4.0', 'keras>=2.1.2', 'scipy', 
+                        'numpy', 'numpy', 'pandas',
+                        'scikit-learn>=0.18.2', 'h5py'],
+      extras_require={'plots':  ["matplotlib"]}
+      )
