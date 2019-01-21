f31c0186739b1dbfd689614323a5cb9b58a63811
Jonathan Fraine <jdfraine@gmail.com>
2019-01-21 14:54:46 +0200

    worked with example datasets

--- examples/libras_model.py
+++ examples/libras_model.py
@@ -193,6 +193,6 @@ def squeeze_excite_block(input):
 if __name__ == "__main__":
     model = generate_model_2()
 
-    # train_model(model, DATASET_INDEX, dataset_prefix='libras_', epochs=1000, batch_size=128)
+    train_model(model, DATASET_INDEX, dataset_prefix='libras_', epochs=1000, batch_size=128)
 
     evaluate_model(model, DATASET_INDEX, dataset_prefix='libras_', batch_size=128)
